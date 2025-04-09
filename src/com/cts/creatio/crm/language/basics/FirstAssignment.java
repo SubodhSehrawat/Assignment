7f8a832dedc81d020e46ceb5719136a6cd74d9f5
SubodhSehrawat <SubodhSehrawat55@gmail.com>
2025-04-09 20:42:00 +0530

    FirstAssignment

---
+++ src/com/cts/creatio/crm/language/basics/FirstAssignment.java
@@ -0,0 +1,114 @@
+package com.cts.creatio.crm.language.basics;
+
+public class FirstAssignment {
+
+	public static void main(String[] args) {
+		// Temperature of a city in degrees Celsius: 25.5
+		
+		double celsius = 25.5;
+		System.out.println(celsius);
+		
+		//Whether a customer has placed an order: true or false
+		
+		boolean customer = true ;
+				System.out.println(customer);
+
+		//Person's phone number: "123-456-7890"
+				
+		String Phonenumber = "123-456-7890";
+		System.out.println(Phonenumber);
+		
+		//Amount of money in a customer's bank account: 1000.50
+		
+		double bankaccount = 1000.50;
+		System.out.println(bankaccount);
+		
+		//Person's email address:"john.doe@example.com"
+		
+		String string = "john.doe@example.com";
+		System.out.println(string);
+		
+		//Coordinates of a location (latitude, longitude): 37.7749, -122.4194
+		
+		String latitude = "37.7749";
+		String longitude = "-122.4194";
+		System.out.println(latitude + longitude);
+		
+		//Person's marital status: true or false
+		
+		boolean maritalstatus = true;
+		System.out.println(maritalstatus);
+		
+		//Person's occupation: "Software Engineer"
+		
+		String occupation = "Software Engineer";
+		System.out.println(occupation);
+		
+		//Person's favourite colour: "Blue"
+		
+		String favouritecolour ="Blue";
+		System.out.println(favouritecolour);
+		
+		//Current year: 2023
+		
+		int currentyear = 2023;
+		System.out.println(currentyear);
+		
+		//Number of followers on a social media platform: 1,000,000
+		
+		long followers =1000000;
+		System.out.println(followers);
+		
+		//Rating of a movie: 7.5
+		double rating = 7.5;
+		System.out.println(rating);
+		
+		//Person's blood type: 'A'
+		
+		char  bloodtype = 'A';
+		System.out.println(bloodtype);
+		
+		//Title of a book: "To Kill a Mockingbird"
+        String Book = "To Kill a Mockingbird";
+        System.out.println("Title of a"+ Book);
+        
+        //Number of employees in a company: 500
+
+        int employees=500 ;
+        System.out.println(employees);
+        
+        //Time of an event: 2:30 PM
+        
+        String eventTime = "2:30 PM";
+        System.out.println(eventTime);
+        
+        //Name of a country: "United States"
+        
+         String country = "United States";
+         System.out.println(country);
+         
+         //Person's eye color: "Brown"
+         String eyecolour = "Brown";
+         System.out.println(eyecolour);
+         
+         //Person's birthplace: "New York City"
+         String birthplace = "New York City";
+         System.out.println(birthplace);
+         
+         //20.Distance between two cities: 200.5
+         
+         double twocities=200.5;
+         System.out.println(twocities);
+         
+         
+         
+         
+        
+         
+        
+		
+		
+		
+	}
+
+}
